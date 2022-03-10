c6e4629cd5d52625f6a2f9969458cc8fb362db9e
WBbug <1056342711@qq.com>
2022-03-10 10:57:41 +0800

    sec

--- src/api/base.js
+++ src/api/base.js
@@ -5,8 +5,8 @@ const base ={
     covInfo:'txapi/ncov/index?key=b5919dd6f573907e378d0a6be7a78ff3',
 
     baseUrl:'https://vyps.api.storeapi.net/api',
-    chinaData:'/94/219?format=json&appid=14558&sign=c3ad57880b68fe078b7de7fcafa666c4',//sign=c3ad57880b68fe078b7de7fcafa666c4国内疫情
-    worldData:'/94/220?format=json&appid=14558&sign=c3ad57880b68fe078b7de7fcafa666c4',//sign=c3ad57880b68fe078b7de7fcafa666c4' 国外
+    chinaData:'/94/219?format=json&appid=14558&',//sign=c3ad57880b68fe078b7de7fcafa666c4国内疫情
+    worldData:'/94/220?format=json&appid=14558&',//sign=c3ad57880b68fe078b7de7fcafa666c4' 国外
     cityData:'/94/221?',//城市数据
     getSwiper:'http://iwenwiki.com/wapicovid19/ncovimg.php',//轮播图的图
     policy:'https://file1.dxycdn.com/2021/0127/319/0185768311460321643-135.json?t=27445773'//防疫政策
